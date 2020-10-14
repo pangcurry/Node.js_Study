4249cec4506e9f0afaec1e3a9b434e6f396124a0
pangcurry <magicshuffle2424@hotmail.com>
2020-10-14 23:35:18 +0900

    [FIX] 토큰 뽑기 수정

--- JWT/src/1012-ex1.js
+++ JWT/src/1012-ex1.js
@@ -14,3 +14,9 @@ const bearer = await bearerHeader.substr(7,143);
 
 // 만약 error중에서 split is not defined 와 같은 기본 함수 오류가 뜬다면
 // 해당 문자열이 비어있어 error가 뜰 가능성이 있다.
+
+// 1014 추가
+// 토큰을 받을 때 substr()로 길이를 구하고 자르기 보단,
+// split()으로 배열에 담은 다음 인덱스 1번을 뽑아 토큰으로
+// 만드는 것이 좋다.
+
